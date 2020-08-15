6dd33c87c312637e20d52c09d8569b5f4fdd1f27
mlsandoval <michaelsandoval8@gmail.com>
2020-08-15 01:57:59 +0000

    changing personal data begun, images added, BG chosen

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,6 +7,7 @@ class About extends Component {
       var name = this.props.data.name;
       var profilepic= "images/"+this.props.data.image;
       var bio = this.props.data.bio;
+      var bio2 = this.props.data.bio2;
       var street = this.props.data.address.street;
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
@@ -25,7 +26,8 @@ class About extends Component {
          <div className="nine columns main-col">
             <h2>About Me</h2>
 
-            <p>{bio}</p>
+            <p>{bio + "\n"}</p>
+            <p>{bio2}</p>
             <div className="row">
                <div className="columns contact-details">
                   <h2>Contact Details</h2>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,19 +5,31 @@ class Header extends Component {
   render() {
 
     if(this.props.data){
-       var project = this.props.data.project;
-       var github = this.props.data.github;
+      var project = this.props.data.project;
+      var github = this.props.data.github;
       var name = this.props.data.name;
       var description= this.props.data.description;
       var city= this.props.data.address.city;
       var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+      return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
       })
     }
 
     return (
       <header id="home">
-      <ParticlesBg type="circle" bg={true} />
+      {/* "color"
+      "ball"
+      "lines"
+      "thick"
+      "circle"
+      "cobweb"
+      "polygon"
+      "square"
+      "tadpole"
+      "fountain"
+      "random"
+      "custom" */}
+      <ParticlesBg className="THISISPARTICLESBG" type="lines" bg={true} />
       <nav id="nav-wrap">
          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
 	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
@@ -35,7 +47,7 @@ class Header extends Component {
       
          <div className="banner-text">
             <h1 className="responsive-headline">{name}</h1>
-            <h3>{description}.</h3>
+            <h3>{description}</h3>
             <hr />
             <ul className="social">
                <a href={project} className="button btn project-btn"><i className="fa fa-book"></i>Project</a>
